e8a5d936c125c9e9d9ccfa79bb2bc03e3cce16f5
Oleksandr <chigarev.oleksandr@lll.kpi.ua>
2026-01-13 20:44:15 +0000

    Add generated patches

--- calculator.cpp
+++ calculator.cpp
@@ -1,3 +1,15 @@
+/**
+ * Calculator Project
+ * * This is a simple calculator implementation class.
+ * It currently supports the following operations:
+ * - Addition (Add)
+ * - Subtraction (Sub)
+ * - Multiplication (Mul)
+ * * Future improvements may include division and advanced math functions.
+ * * Lab work performed by: Oleksandr
+ * Date: 2026
+ * * Keep coding and stay safe!
+ */
 #include "calculator.h"
 
 int Calculator::Add (double a, double b)
